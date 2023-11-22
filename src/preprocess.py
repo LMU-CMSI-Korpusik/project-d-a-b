f13b71c930e801ea722e47396697b5c90dac821e
abemo <amooreod@lion.lmu.edu>
2023-11-21 17:44:23 -0800

    featurized train_essays and train_prompts

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,39 +1,46 @@
 import pandas as pd
 import numpy as np
-import torch
-import torch.nn as nn
-
-if torch.cuda.is_available():
-    device = 'cuda'
-else:
-    device = 'cpu'
+from sklearn.feature_extraction.text import CountVectorizer
 
 
-class Net(nn.Module):
-    def __init__(self, num_input, num_out):
-        super().__init__()
-        # only a single input layer then a sigmoid output layer
-        # can create attribute for each hidden layer
-        self.input = nn.Linear(num_input, num_out)
-        self.h_1 = nn.Linear(num_input, num_out)
-        self.h_2 = nn.Linear(num_input, num_out)
-        self.h_3 = nn.Linear(num_input, num_out)
-        self.sigmoid = nn.Sigmoid()
-    
-    def forward(self, input):
-        return  self.sigmoid(self.h_3(self.sigmoid(self.h_2(self.sigmoid(self.h_1(self.input(input)))))))
+def featurize():
+    # import train data and prompts as pandas dataframes
+    train_essays = pd.read_csv("data/train_essays.csv")
+    prompts = pd.read_csv("data/train_prompts.csv")
 
-# import train data and prompts as pandas dataframes
-train_essays = pd.read_csv("data/train_essays.csv")
-prompts = pd.read_csv("data/train_prompts.csv")
+    # create a dataframe with columns (essay_text, instructions, source_text, generated) with length equal to train_essays length
+    corpus = pd.DataFrame(columns=["essay_text", "instructions", "source_text", "generated"], index=np.arange(len(train_essays)))
 
-# create numpy feature vectors
+    for index, row in train_essays.iterrows():
+        essay_text = row["text"]
+        prompt_id = row["prompt_id"]
+        instructions = prompts.loc[prompts["prompt_id"] == prompt_id]["instructions"].values[0]
+        source_text = prompts.loc[prompts["prompt_id"] == prompt_id]["source_text"].values[0]
+        generated = row["generated"]
+        corpus.loc[index] = {"essay_text": essay_text, "instructions": instructions, "source_text": source_text, "generated": generated}
 
-# convert numpy feature vectors to torch tensors
+    # encode feature vectors of essay text, instructions, and source text
+    # to create vectors < <essay_text_features>, <instruction_features>, <source_text_features>, generated >
+    # and put into a numpy array
+
+    features = np.zeros((len(corpus), 4), dtype="object")
+    for index, row in corpus.iterrows():
+        essay_text = row["essay_text"]
+        instructions = row["instructions"]
+        source_text = row["source_text"]
+        generated = row["generated"]
+        essay_text_features = CountVectorizer().fit([essay_text]).transform([essay_text]).toarray()
+        instructions_features = CountVectorizer().fit([instructions]).transform([instructions]).toarray()
+        source_text_features = CountVectorizer().fit([source_text]).transform([source_text]).toarray()
+        features[index,:] = np.array([essay_text_features, instructions_features, source_text_features, generated], dtype="object")
+
+    np.save("data/features.npy", features)
+
     
 
 def main():
-    return 
+    # featurize()
+    features = np.load("data/features.npy", allow_pickle=True)
     
 
 if __name__ == "__main__":
